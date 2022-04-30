9807abc829f728986d8079434c6aaca764ad0c7d
David <depch47@gmail.com>
2022-04-30 09:14:47 -0500

    ADD---Videogame GET controller

--- src/main/java/com/davinchi/videogameapi/controller/VideogameController.java
+++ src/main/java/com/davinchi/videogameapi/controller/VideogameController.java
@@ -1,4 +1,27 @@
 package com.davinchi.videogameapi.controller;
 
+import com.davinchi.videogameapi.entity.Videogame;
+import com.davinchi.videogameapi.service.VideogameServiceImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("api/v1/videogames")
 public class VideogameController {
+
+    private final VideogameServiceImplementation videogameServiceImplementation;
+
+    @Autowired
+    public VideogameController(VideogameServiceImplementation videogameServiceImplementation) {
+        this.videogameServiceImplementation = videogameServiceImplementation;
+    }
+
+    @GetMapping
+    public List<Videogame> getAllVideogames() {
+        return videogameServiceImplementation.getAllVideogames();
+    }
 }
