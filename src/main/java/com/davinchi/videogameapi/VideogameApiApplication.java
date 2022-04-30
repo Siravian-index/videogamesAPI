14ca6b546b2868fa73ed2511c539854a40288132
David <depch47@gmail.com>
2022-04-30 08:44:19 -0500

    starter files

---
+++ src/main/java/com/davinchi/videogameapi/VideogameApiApplication.java
@@ -0,0 +1,13 @@
+package com.davinchi.videogameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogameApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideogameApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/davinchi/videogameapi/VideogameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davinchi.videogameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogameApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
